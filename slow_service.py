ce5d104d6e59ce830a65af2a2c247823f5a8a4a7
francesco scorca <70110839+jemjacceit@users.noreply.github.com>
2022-02-03 15:23:50 +0100

    Add files via upload

--- slow_service.py
+++ slow_service.py
@@ -29,7 +29,7 @@ class Classifier(object):
         self.frame_step = 320
         self.lower_frequency = 20
         self.upper_frequency = 4000 
-        self.num_mel_bins = 40
+        self.num_mel_bins = 32
         self.num_coefficients = 10
         self.num_spectrogram_bins = (self.frame_length) // 2 + 1
         self.linear_to_mel_weight_matrix  = tf.signal.linear_to_mel_weight_matrix(self.num_mel_bins, 
@@ -38,7 +38,8 @@ class Classifier(object):
                                                                                   self.lower_frequency,
                                                                                   self.upper_frequency)
                 
-        #Preparing tflite model
+        # Preparing tflite model
+
         self.interpreter = tflite.Interpreter('kws_dscnn_True.tflite')
         self.interpreter.allocate_tensors()
         self.input_details = self.interpreter.get_input_details()
@@ -63,24 +64,23 @@ class Classifier(object):
 
         body = cherrypy.request.body.read()
         body = json.loads(body)
-        audio_binary = b64decode(body.get("audio"))
         
-    
+        audio_binary = b64decode(body.get('e')[0].get('v'))
         audio, _ = tf.audio.decode_wav(audio_binary)
         audio = tf.squeeze(audio, axis=1)
         audio = tf.convert_to_tensor(audio, dtype=tf.float32)
         
-        #padding
+        # Padding
         zero_padding = tf.zeros([self.sampling_rate] - tf.shape(audio), dtype=tf.float32)
         audio = tf.concat([audio, zero_padding], 0)
         audio.set_shape([self.sampling_rate])
 
-        #stft
+        # STFT
         stft = tf.signal.stft(audio, frame_length=self.frame_length,
                 frame_step=self.frame_step, fft_length=self.frame_length)
         spectrogram = tf.abs(stft)
 
-        #mfcc
+        # MFCC
         mel_spectrogram = tf.tensordot(spectrogram,
                 self.linear_to_mel_weight_matrix, 1)
         log_mel_spectrogram = tf.math.log(mel_spectrogram + 1.e-6)
@@ -91,13 +91,13 @@ class Classifier(object):
 
     
     
-        #Predict keyword in input 
+        # Predict keyword in input 
         self.interpreter.set_tensor(self.input_details[0]['index'], mfccs.numpy())
         self.interpreter.invoke()
         output = self.interpreter.get_tensor(self.output_details[0]['index'])
                     
             
-        #Sending data 
+        # Sending data 
         output = {'keyword': self.commands[tf.math.argmax(output[0])]}
         output_str = json.dumps(output)
         return output_str
@@ -110,8 +110,8 @@ class Classifier(object):
 if __name__== '__main__':
     conf = {'/':{'request.dispatch': cherrypy.dispatch.MethodDispatcher()}}
     cherrypy.tree.mount(Classifier(), '', conf)
-    cherrypy.config.update({'server.socket_host': '0.0.0.0'})
+    # Change with server IP
+    cherrypy.config.update({'server.socket_host': '169.254.104.205'})
     cherrypy.config.update({'server.socket_port': 8080})
     cherrypy.engine.start()
     cherrypy.engine.block()
-
